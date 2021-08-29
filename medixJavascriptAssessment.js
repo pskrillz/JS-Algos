7f8cab2e939c5f8da36aae809c2129d629511e62
pi <payam.farah@live.com>
2021-08-29 15:53:25 -0700

    finished some more questions

--- medixJavascriptAssessment.js
+++ medixJavascriptAssessment.js
@@ -119,26 +119,31 @@ undefined
 * JavaScript concepts: variable scope, hosting, ES6
 */
 
-function userDetails(username) {
-  if (username) {
-    console.log(salary);
-    console.log(age);
+// function userDetails(username) {
+//   if (username) {
+//     console.log(salary);
+//     console.log(age);
 
-    let age = 30;
-    var salary = 10000;
-  }
+//     let age = 30;
+//     var salary = 10000;
+//   }
 
-  console.log(salary);
-  console.log(age);
-}
+//   console.log(salary);
+//   console.log(age);
+// }
 
-userDetails('John'); // What is the output?
-30
-10000
+// userDetails('John'); // What is the output?
+// Original Answer
+// 30
+// 10000
 
-Undefined
-Undefined
+// Undefined
+// Undefined
 
+// Answer: Incorrect
+
+// Correct Answer:
+// undefined, reference error, variable must be before console log in the local scope
 
 
 
@@ -173,7 +178,14 @@ var interval = setInterval(function () {
 
 greeting(); // What is the output? 
 
-'Hi from greeting function'
+// Original Answer: 'Hi from greeting function'
+// Incorrectl;
+
+// Correct Answer: 
+// Hi from global
+// Hi from greeting function
+// Hi from setInterval
+// Hi from setTimeout
 
 
 
@@ -187,82 +199,149 @@ greeting(); // What is the output?
 
 
 
+// Second List of Challenges:
 
 
+/**
+ * Challenge 1 - Reverse a string
+ *
+ * Create a function that takes a string as an argument and return the string as reversed
+ * @example reverseString('this is a string') // 'gnirts a si siht'
+ */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Test 1 Question 1
-
-
-var text = 'outside';
-
-function logText() {
-  console.log(text);
-
-  // so if i change the order of the inner var to before the console log, itll work!
-  var text = 'inside';
+ function reverseString(string) {
+	let answer = string.split('').reverse().join('')
 }
 
-logText(); // What is the output?
-// correct answer = undefined
 
+reverseString('this is a string'); // 'gnirts a si siht'
+reverseString('Backward'); // 'drawkcaB'
 
-
-//////////////////////////////
-
-
-// Other example
-// var test = "I'm global";
-
-// function testScope() {
-//   var test = "I'm local";
-
-//   console.log (test);     
-// }
-
-// testScope();           // output: I'm local
-
-// console.log(test);
+// Answer: Incorrect, just needed return to be correct. 
 
 
 
 
 
-//// TODO: Analyze take-aways later.
+
+/**
+ * Challenge 2 - Get unique values of an array
+ *
+ * Create a function that takes an array as an argument and return the array with unique values
+ * @example uniqueArrayValues([5, 5, 7, 6, 2, 4, 6, 2]) // [5, 7, 6, 2, 4]
+ */
+
+function uniqueArrayValues(array) {
+  let uniques = []
+	for(let i = 0; i < array.length; i++){
+    if(!(uniques.includes(array[i]))){
+      uniques.push(array[i])
+    }
+   
+  }
+    return uniques
+
+}
+
+
+console.log(uniqueArrayValues([5, 5, 7, 6, 2, 4, 6, 2])); // [5, 7, 6, 2, 4]
+uniqueArrayValues([100, 5, 74, 74, 100]); // [100, 5, 74]
+
+// Incorrect: did not know the answer
+// All I needed was array method .includes. 
+
+
+
+
+
+
+/**
+ * Challenge 3 - Get the sum of an array values
+ *
+ * Create a function that takes an array as an argument and return the sum of the array values
+ * @example getSumValue([5, 82, 2, 100]) // 189
+ */
+
+
+function getSumValue(array) {
+  let total = 0;
+	for(let num of array){
+    total += num
+  }
+  return total;
+}
+
+
+console.log(getSumValue([5, 82, 2, 100])) // 189
+// getSumValue([7, 1.34, 1.2]) // 9.54
+
+// Incorrect, array was spelled 'arr' not 'array;
+// also total needed to be instantiated
+//but the right idea was there. 
+
+
+
+
+
+
+
+/**
+ * Challenge 4 - Filter key/value that does not have integer value
+ *
+ * Create a function that takes an object as an argument and remove key/value with value that is not an integer
+ * @example filterObject({ a: 'a string', b: 123, c: null, d: 3.14 }) // { b: 123 }
+ */
+
+
+function filterObject(object) {
+  // let filteredObject;
+
+  // object.entries((item) => {
+  //   if (item.isNumber()){
+  //     filtereditem
+  //   }
+  // })
+// let filtered;
+
+//   for(let prop in object){
+//     if(typeof prop == 'number'){
+//       filtered.prop = prop
+//     }
+//   }
+//   return filtered;
+
+Object.keys(object).forEach(key => {
+  if (typeof object[key] !== 'number' || !Number.isInteger(object[key])) delete object[key];
+});
+return object;
+
+}
+
+
+console.log(filterObject({ a: 'a string', b: 123, c: null, d: 3.14 })); // { b: 123 }
+// filterObject({ d7: '3.14', b3: 6, a6: 2021, a2: 0 }); // { b3: 6, a6: 2021 }
+// Wrong, but had a good general idea..
+// Here is the correct answer.
+
+
+
+
+
+
+/**
+ * Challenge 5 - Deep clone an object
+ *
+ * Create a function that takes an object as an argument and return the deeply cloned object
+ * @example deepCloneObject({ a: 'A', b: { c: {  d: 'nested' } } }) // { a: 'A', b: { c: {  d: 'nested' } } }
+ */
+
+
+function deepCloneObject(object) {
+	let deepClone = {}
+  
+
+}
+
+// deepCloneObject({ a: 'A', b: { c: {  d: 'nested' } } }); // { a: 'A', b: { c: {  d: 'nested' } } }
+
